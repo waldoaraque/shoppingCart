c033283cc56f52b67f3d530e4120284112912623
waldev <waldoaraque10@gmail.com>
2020-03-29 18:08:31 +0200

    adding the function to add the courses at the shopping car

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //Vars
 const car = document.getElementById('carrito');
 const courses = document.getElementById('lista-cursos');
+const coursesList = document.querySelector('#lista-carrito tbody');
 
 //Listeners
 getEventListeners();
@@ -21,5 +22,29 @@ function buyCourses(e){
 }
 
 function dataCourse(course){
+    const infoCourse = {
+        image: course.querySelector('img').src,
+        tittle: course.querySelector('h4').textContent,
+        price: course.querySelector('.precio span').textContent,
+        id: course.querySelector('a').getAttribute('data-id')
+    }
+
+    setCourse(infoCourse);
+
     console.log(course);
+}
+
+function setCourse(data){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${data.image}" width="100"> 
+        </td>
+        <td> ${data.tittle} </td>
+        <td> ${data.price} </td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${data.id}">X</a>
+        </td>
+    `;
+    coursesList.appendChild(row);
 }
